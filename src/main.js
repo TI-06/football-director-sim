fb3a85ea7ce1a41ba807a76859ae9620a5b70fd0
TI-06 <iretaku.shop@gmail.com>
2026-07-31 11:05:09 +0900

    feat: isolate redesigned save storage

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-import { boot } from './ui/controller.js';
+const LEGACY_STORAGE_KEY = 'football-director-save-v2';
+const ACTIVE_STORAGE_KEY = 'football-director-save-v4';
 
+const storageGetItem = Storage.prototype.getItem;
+const storageSetItem = Storage.prototype.setItem;
+const storageRemoveItem = Storage.prototype.removeItem;
+
+function activeKey(key) {
+  return key === LEGACY_STORAGE_KEY ? ACTIVE_STORAGE_KEY : key;
+}
+
+Storage.prototype.getItem = function getItem(key) {
+  return storageGetItem.call(this, activeKey(key));
+};
+
+Storage.prototype.setItem = function setItem(key, value) {
+  return storageSetItem.call(this, activeKey(key), value);
+};
+
+Storage.prototype.removeItem = function removeItem(key) {
+  return storageRemoveItem.call(this, activeKey(key));
+};
+
+const { boot } = await import('./ui/controller.js');
 boot();
